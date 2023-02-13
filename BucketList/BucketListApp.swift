e76eb5cc688d8549da6c665b9e03bfa226dab449
Maurício Costa <86126994+mauriciomunky@users.noreply.github.com>
2023-02-13 16:09:29 -0300

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Maurício Costa on 13/02/23.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
